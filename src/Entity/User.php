b032e290c36cb2b9c8206caae18f19ab0060d976
BR-Sywar <benromdhanesywar@gmail.com>
2020-02-19 15:28:55 +0100

    ajouter les attribut à User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,9 +19,133 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $nom;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $prenom;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $ville;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $adresse;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $lng;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $lat;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $tel;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getLng(): ?string
+    {
+        return $this->lng;
+    }
+
+    public function setLng(string $lng): self
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+
+    public function getLat(): ?string
+    {
+        return $this->lat;
+    }
+
+    public function setLat(string $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
     }
 }
